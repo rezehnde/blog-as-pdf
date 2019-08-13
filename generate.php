60f80e47d0aee1dfeb169954ddf0c5df8bc3daa4
Marcos Gugeler <marcosgugeler@gmail.com>
2019-08-13 17:10:05 -0300

    First version

---
+++ generate.php
@@ -0,0 +1,64 @@
+<?php
+require_once("../../../wp-config.php");
+require_once('tcpdf/config/lang/eng.php');
+require_once('tcpdf/tcpdf.php');
+
+ob_end_clean();
+
+// create new PDF document
+$objTcpdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true); 
+// set document information
+$objTcpdf->SetCreator(PDF_CREATOR);
+$objTcpdf->SetTitle( get_option('blogname') );
+// set default header data
+$objTcpdf->SetHeaderData(null, null, get_option('blogname'), get_option('blogdescription') . "\n" . get_option('home') . "\n" );
+// set header and footer fonts
+$objTcpdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+$objTcpdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+//set margins
+$objTcpdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+$objTcpdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+$objTcpdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+//set auto page breaks
+$objTcpdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+//set image scale factor
+$objTcpdf->setImageScale(PDF_IMAGE_SCALE_RATIO); 
+//set some language-dependent strings
+$objTcpdf->setLanguageArray($l); 
+//initialize document
+$objTcpdf->AliasNbPages();
+
+global $wpdb, $post;
+//$query = "SELECT * FROM ".$wpdb->posts;
+//$results = $wpdb->get_results($query);
+
+$category_id = $_GET['category_id'];
+
+$results = get_posts('numberposts=1000&category='.$category_id);
+
+foreach ($results as $objPost) {
+
+	$content = $objPost->post_content;  
+	$postOutput = preg_replace('/<img[^>]+./','', $content);
+	
+	$pos = strpos($postOutput, '<table');
+	if ($pos == true) continue;
+	
+	// add a page
+	$objTcpdf->AddPage();
+	
+	// set font
+	$objTcpdf->SetFont( PDF_FONT_NAME_MAIN, '', 10 );
+	
+	// ---------------------------------------------------------
+	
+
+	$strHtml = '<h1>' . $objPost->post_title . '</h1>' . wpautop( '<b>' . $objPost->post_date . '</b><br/>' . $postOutput, true );
+	
+	// output the HTML content
+	$objTcpdf->writeHTML( $strHtml , true, 0, true, 0);
+}
+
+//output PDF document
+$objTcpdf->Output( get_option('blogname') . '.pdf', 'D' );
+?>
---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== Blog as PDF ===
+Contributors: rezehnde
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5RJD5395AA47U&source=url
+Tags: pdf, posts, ebook
+Requires at least: 4.0
+Tested up to: 5.0.2
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Export posts from your wordpress blog as PDF.
+
+== Description ==
+
+Export posts from your wordpress blog as PDF selecting some categories or downloading all posts,
+
+<em>This plugin is dedicated to Deise Daiana Gugeler Bazanella, my wonderful wife.</em>
+
+== Installation ==
+
+1. Unzip `blog-as-pdf.zip` inside the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' admin menu in WordPress
+1. Export your blog as PDF in the WordPress admin via `Configurations -> Blog as PDF`
+
+== Changelog ==
+
+0.2 * Added 'export posts from categories' selecting only a single category
+1.0 Plugin review after nine years of abandon
